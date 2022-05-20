7cf9e6e6c4c143aff2343df8fd85c357f384dca6
unknown <aluno@L4M16.IPIRANGA.FATEC>
2022-05-20 20:14:12 -0300

    Aula PDM - redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+//()=>
+//nome - criarContrato
+//recebe: nome e valor
+//devolve: um JSON com type = CRIAR_CONTRATO e payload igual a um JSON com nome e valor
+
 
 function App() {
+
+   const criarContrato = (nome, valor) =>{
+      return {
+        type : "CRIAR_CONTRATO",
+        payload : {
+            nome, valor
+        } 
+      }
+   }  
+
+//function regular (usar function)
+//cancelarContrato
+//recebe : nome
+// devolve: um JSON com type = CANCELAR_CONTRATO e payload igual a um JSON com nome
+
+const cancelarContrato = (nome) =>{
+  return {
+    type : "CANCELAR_CONTRATO",
+    payload : {
+        nome
+    } 
+  }
+} 
+// uma função criadora de ação para solicitação de cashback
+const SolicitarCashBack = (valor, nome) =>{
+  return {
+    type : "SOLICITAR_CASHBACK",
+    cashback : {
+        valor, nome
+    } 
+  }
+} 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
